aff2adcb28da63ae2141816f0dea82850c08e5f7
RajKarkee <karkeemiraj@gmail.com>
2025-11-15 19:05:47 +0545

    refactor: Update admin retrieval logic in DashboardController

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -19,7 +19,11 @@ class DashboardController extends Controller
         //     abort(403, 'Admin session missing.');
         // }
 
-        $admin = Admin::with(['restaurants.staff'])->findOrFail(Auth::id());
+        // $admin = Admin::with(['restaurants.staff'])->findOrFail(Auth::id());
+        $admin = DB::table('admins')->
+        leftjoin('restaurants', 'restaurants.owner_id', 'admins.id')->
+        where('id', Auth::id())->first();
+    
         $restaurants = $admin->restaurants()->with('staff')->orderByDesc('created_at')->get();
 
         $currentRestaurant = null;
