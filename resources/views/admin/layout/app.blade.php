4cf72d5f59c270b3260801634343ef2166aae6ab
RajKarkee <karkeemiraj@gmail.com>
2025-11-19 19:50:44 +0545

    feat: Add profile modal styles and integrate modal into main stylesheets

--- resources/views/admin/layout/app.blade.php
+++ resources/views/admin/layout/app.blade.php
@@ -7,9 +7,9 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Admin Panel - Dashboard')</title>
 
-    <!-- Bootstrap 5 CSS -->
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    {{-- searchable --}}
+
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 
 
@@ -19,7 +19,7 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
 
     <link rel="stylesheet" href="{{ asset('css/admin/index.css') }}">
-    <style>
+    {{-- <style>
         .profile-modal-overlay {
             position: fixed;
             z-index: 9999;
@@ -262,7 +262,7 @@
                 flex-direction: column;
             }
         }
-    </style>
+    </style> --}}
     @stack('styles')
 </head>
 
